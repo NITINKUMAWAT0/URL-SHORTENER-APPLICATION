3c8843e5e946a628b7d20c4f65f9fa7e023bf3ec
Nitin Kumawat <nitin.kumawat.jaipur.in@gmail.com>
2024-10-08 19:32:35 +0530

    made changes in the backend

--- url-shortener-backend/db.js
+++ url-shortener-backend/db.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER, 
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME  
+  host: 'localhost',
+  user: 'root', 
+  password: '1234oooo',
+  database: 'url_shortener' 
 });
 
 connection.connect((err) => {
